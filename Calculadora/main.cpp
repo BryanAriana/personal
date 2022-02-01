71e335dedeecf9d314b4101e4941b9409f9c8113
BryanAriana <98106320+BryanAriana@users.noreply.github.com>
2022-01-31 19:56:21 -0600

    Update main.cpp

--- Calculadora/main.cpp
+++ Calculadora/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    int numero1 = 0;
-    int numero2 = 0;
+    double numero1 = 0;
+    double numero2 = 0;
     cout << "Introduce tu primer numero: ";
     cin >> numero1;
     cout << "Introduce tu segundo numero: " << endl;
