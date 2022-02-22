7ab9c0b5159485e0688be59f1f033a9cdc2e41e1
BryanAriana <98106320+BryanAriana@users.noreply.github.com>
2022-02-21 19:19:57 -0600

    Add files via upload

--- Juego-robot/Juego-robot.cpp
+++ Juego-robot/Juego-robot.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
-void drawMap(int posX,int posY,char gameMap[10][10]){
+void SaveFile(string Nombre, char map[10][10])
+{
+    ofstream FileMap("Mapa.txt");
+    if(FileMap.is_open())
+    {
+        FileMap << Nombre << endl;
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 10; j++)
+            {
+               FileMap << map[i][j];
+            }
+            FileMap << endl;
+        }
+    }
+    FileMap.close();
+    }
+void drawMap(int posX,int posY,char map[10][10]){
   for(int i=0;i<10;i++){
     for(int j=0;j<10;j++){
       if(posX==j && posY==i)
@@ -9,28 +28,64 @@ void drawMap(int posX,int posY,char gameMap[10][10]){
         }
       else
       {
-            cout<<gameMap[i][j];
+            cout<<map[i][j];
       }
     }
     cout<<endl;
   }
 }
+string OpenFile(char map[10][10])
+{
+    ifstream FileMap("Mapa.txt");
+    string line;
+    string name="No encontre nombre";
+    int renglon = 0;
+    int row = 0;
+
+    if(FileMap.is_open())
+    {
+        while(getline(FileMap, line))
+        {
+            if(renglon == 0)
+                name = line;
+            else
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                        map[row][i] = line[i];
+                }
+                row++;
+            }
+            renglon++;
+        }
+
+        FileMap.close();
+    }
+    else
+    {
+        cout << "Error. No se pudo abrir el archivo." << endl;
+    }
+    return name;
+    }
 int main(){
   int posX=3;
   int posY=3;
-  char map[10][10]={{'0','0','0','0','0','0','0','0','0','0'},
-		  {'0','0','0','0','0','0','0','0','0','0'},
-		  {'0','0','0','0','0','0','0','0','0','0'},
-		  {'0','0','0','0','0','0','0','0','0','0'},
-		  {'0','0','0','0','0','0','0','0','0','0'},
-		  {'0','0','0','0','0','0','0','0','0','0'},
-		  {'0','0','0','0','0','0','0','0','0','0'},
-		  {'0','0','0','0','0','0','0','0','0','0'},
-		  {'0','0','0','0','0','0','0','0','0','0'},
-		  {'0','0','0','0','0','0','0','0','0','0'}};
+  char map[10][10]={{'1','1','1','1','1','1','1','1','1','1'},
+		  {'1','0','0','0','0','0','0','0','0','1'},
+		  {'1','0','0','0','0','0','0','0','0','1'},
+		  {'1','0','0','0','0','0','0','0','0','1'},
+		  {'1','0','0','0','0','0','0','0','0','1'},
+		  {'1','0','0','0','0','0','0','0','0','1'},
+		  {'1','0','0','0','0','0','0','0','0','1'},
+		  {'1','0','0','0','0','0','0','0','0','1'},
+		  {'1','0','0','0','0','0','0','0','0','1'},
+		  {'1','1','1','1','1','1','1','1','1','1'}};
 
   char teclado;
   bool gameOver= false;
+  char Input = ' ';
+  string NamePlayer = "";
+  NamePlayer = OpenFile(map);
   drawMap(posX,posY,map);
   while(!gameOver)
   {
@@ -41,19 +96,19 @@ int main(){
     switch (teclado)
     {
         case 'a':
-            if (posX > 0){
+            if (posX > 1){
             posX-=1;}
             break;
         case 'd':
-            if (posX < 9) {
+            if (posX < 8) {
             posX+=1;}
             break;
         case 'w':
-            if (posY > 0) {
+            if (posY > 1) {
             posY-=1;}
             break;
         case 's':
-            if (posY < 9){
+            if (posY < 8){
             posY+=1;}
             break;
         case 'p':
@@ -63,5 +118,6 @@ int main(){
     }
   drawMap(posX,posY,map);
   }
+  SaveFile(NamePlayer, map);
  return 0;
 }
